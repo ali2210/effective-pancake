a6038fb40a3994b2da4d519128c581d8ed5ecee1
Ali Hassan <Alideveloper95@gmail.com>
2021-09-20 23:17:20 +0500

    simple payment using crate

---
+++ target/debug/deps/payPeers-6aef12f4f8ba472d.d
@@ -0,0 +1,5 @@
+/home/ali/Documents/payment/payPeers/target/debug/deps/payPeers-6aef12f4f8ba472d: src/main.rs
+
+/home/ali/Documents/payment/payPeers/target/debug/deps/payPeers-6aef12f4f8ba472d.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/payPeers.d
@@ -0,0 +1,1 @@
+/home/ali/Documents/payment/payPeers/target/debug/payPeers: /home/ali/Documents/payment/payPeers/src/main.rs
